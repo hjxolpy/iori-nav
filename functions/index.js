946cd8f25f293a28be7291b1597c1beb34ae145b
hjxolpy <76271814+hjxolpy@users.noreply.github.com>
2026-09-07 18:12:37 +0800

    Replace Google option with Bing in search engines

--- functions/index.js
+++ functions/index.js
@@ -252,7 +252,7 @@ export async function onRequest(context) {
   const searchEngineOptions = S.home_search_engine_enabled ? `
     <div class="flex justify-center items-center gap-3 mb-4 text-sm select-none search-engine-wrapper">
         <label class="search-engine-option active" data-engine="local"><span>站内</span></label>
-        <label class="search-engine-option" data-engine="google"><span>Google</span></label>
+        <label class="search-engine-option" data-engine="bing"><span>Bing</span></label>
         <label class="search-engine-option" data-engine="baidu"><span>Baidu</span></label>
         <label class="search-engine-option" data-engine="github"><span>GitHub</span></label>
     </div>` : '';
